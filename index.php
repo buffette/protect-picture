db5ce93ba0e42d118f7d6c72cf5c77f3e1adceab
buffette <139962583+buffette@users.noreply.github.com>
2023-07-29 16:54:11 +0200

    exif_read_data function only for image/jpeg files

--- index.php
+++ index.php
@@ -96,6 +96,9 @@ require_once 'config.php';
 
 <!-- Modal -->
 <div class="modal fade" id="modal-preview" tabindex="-1" aria-labelledby="previewModalLabel" aria-hidden="true">
+    <div class="modal-header">
+    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+    </div>
     <div class="modal-dialog">
 
     </div>
