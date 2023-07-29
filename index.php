27c93a09d9c196d1108ecd0be865feee685e17e9
buffette <139962583+buffette@users.noreply.github.com>
2023-07-29 15:51:40 +0200

    Correct indentation

--- index.php
+++ index.php
@@ -36,9 +36,7 @@ require_once 'config.php';
                 <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z"/>
             </svg>
             <div>
-                <h2>
-                    Le site est passé en version 2 !
-                </h2>
+                <h2>Le site est passé en version 2 !</h2>
                 Voici les principaux changements mis en place pour cette nouvelle version majeur :
                 <ul>
                     <li>Il est désormais possible de prévisuialiser l'image avant le téléchargement, directement dans le formulaire.</li>
